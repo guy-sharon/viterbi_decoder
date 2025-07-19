4ff5cf2b7ab770d5006eacee80ce135fa8251304
guy sharon <g.sharon@proton.me>
2025-07-19 10:29:27 +0300

    enc and dec working, cleaning up

--- main.c
+++ main.c
@@ -1,22 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <assert.h>
+
+// *************************************************** //
+// ********************* defines ********************* //
+// *************************************************** //
+#define MAX(a,b)            ( a > b ? a : b )
+#define MIN(a,b)            ( a < b ? a : b )
 
 #define MAX_POLYNOME_LENGTH             32
 #define MAX_POLYNOMES                   16
+#define MAX_METRIC                      ( UINT_MAX >> 1 )
+#define MAX_DECODE_LEN_BITS             ( 1 << 10 ) // enfore power of 2!
+#define NUM_BITS_IN_INT                 ( 8*sizeof(unsigned int) )
 
-static unsigned long long polynomes[MAX_POLYNOMES] = { 0 };
+// **************************************************** //
+// ********************* typedefs ********************* //
+// **************************************************** //
+typedef unsigned int state_t;
+typedef unsigned int encbit_t;
+typedef unsigned char bit_t;
+
+typedef struct Node
+{
+    unsigned int metric;
+    state_t state;
+    int bits_ind;
+    unsigned int bits[MAX_DECODE_LEN_BITS / NUM_BITS_IN_INT];
+} Node;
+
+
+// ************************************************** //
+// ********************* static ********************* //
+// ************************************************** //
+static unsigned int polynomes[MAX_POLYNOMES] = { 0 };
 static unsigned char polynome_count = 0;
-
-static unsigned long long out;
+static unsigned int depth = 0;
+static unsigned int depth_mask = 0;
+static encbit_t encbit;
+static unsigned int next_node_ind = 0;
 
 #ifdef DEBUG
+    static struct Node nodes[(1<<7) * 50 / 2];
     static unsigned char trellis[1 << 10];
 #else
+    static struct Node *nodes;
     static unsigned char *trellis;
 #endif
 
-const char *program_name = "viterbi_decoder";
+static const char *program_name = "viterbi_decoder";
 
 
 // ************************************************************ //
@@ -29,9 +63,9 @@ void print_help() {
 }
 
 
-// ************************************************** //
-// ********************* Common ********************* //
-// ************************************************** //
+// ************************************************* //
+// ********************* utils ********************* //
+// ************************************************* //
 static void parse_polynomes(int argc, char *argv[])
 {
     polynome_count = argc - 3; // Last argument is bits
@@ -44,20 +78,35 @@ static void parse_polynomes(int argc, char *argv[])
         for (int j = 0; j < strlen(argv[i+2]); j++) {
             polynomes[i] = (polynomes[i] << 1) | (argv[i+2][j] == '1' ? 1 : 0);
         }
+        depth = MAX(depth, 8*sizeof(polynomes[0]) - __builtin_clz(polynomes[i]));
+    }
+
+    depth_mask = 0;
+    for (int i = 0; i < depth; i++)
+    {
+        depth_mask |= 1 << i;
     }
 }
+
 
 // ************************************************** //
 // ********************* Encode ********************* //
 // ************************************************** //
-static void encode_bit(unsigned char bit, long long *state, unsigned long long *out)
+__inline static unsigned int _encode_bit(bit_t bit, state_t state)
 {
-    unsigned long long _out = 0;
-    *state = (*state << 1) | bit;
+    unsigned int _out = 0;
     for (int j = 0; j < polynome_count; j++) {
-        _out = __builtin_parity(*state & polynomes[j]) << j;
+        _out |= __builtin_parity(state & polynomes[j]) << j;
     }
-    *out = _out;
+    return _out;
+}
+
+static state_t encode_bit(bit_t bit, state_t state, encbit_t *encbit)
+{
+    unsigned int _out = 0;
+    state_t next_state = ( (state << 1) | bit ) & depth_mask;
+    *encbit = _encode_bit(bit, next_state);
+    return next_state;
 }
 
 void encode(int argc, char *argv[]) {
@@ -68,48 +117,146 @@ void encode(int argc, char *argv[]) {
     
     char *bits = argv[argc-1];
     int len = strlen(bits);
-    long long state = 0;
+    state_t state = 0;
     for (int i = 0; i < len; i++) {
-        unsigned char bit = bits[i] == '1';
-        encode_bit(bit, &state, &out);
+        bit_t bit = bits[i] == '1';
+        state = encode_bit(bit, state, &encbit);
         for (int j = 0; j < polynome_count; j++) {
-            printf("%d", (unsigned char)((out & (1<<j)) >> j));
+            printf("%d", (bit_t)((encbit & (1<<j)) >> j));
         }
     }
     printf("\n");
 }
 
+
 // ************************************************** //
 // ********************* Decode ********************* //
 // ************************************************** //
+static int hamming_dist(encbit_t in_encbit, encbit_t encbit)
+{
+    return __builtin_popcount(in_encbit ^ encbit);
+}
+
+static void chain_node(struct Node *child, struct Node *parent, bit_t bit, unsigned int ham_dist, state_t state)
+{
+    child->metric = parent->metric + ham_dist;
+    child->state = state;
+    
+    child->bits_ind = parent->bits_ind + 1;
+    unsigned int int_num = child->bits_ind / NUM_BITS_IN_INT;
+    unsigned int bit_offset = child->bits_ind - int_num*NUM_BITS_IN_INT;
+    memcpy(&child->bits, parent->bits, sizeof(child->bits));
+    child->bits[int_num] |= bit << bit_offset;
+}
+
 void decode(int argc, char *argv[]) {
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s -dec <polynomes0> <polynomes1> ... <polynomesN-1> <encoded_bits>\n", program_name);
+        fprintf(stderr, "Usage: %s -dec <polynome0> <polynome1> ... <polynomeN-1> <encoded_bits>\n", program_name);
         return;
     }
 
     // trellis
-    int trellis_size = 1 << polynome_count;
+    int num_states = 1 << depth;
 #ifndef DEBUG
-    trellis = malloc(trellis_size * 2 * sizeof(unsigned char));
+    trellis = malloc(num_states * 2 * sizeof(unsigned char));
 #endif
-    for (long long state = 0; state < trellis_size; state++)
+    for (state_t state = 0; state < num_states; state++)
     {
-        long long _state = state;
-        for (unsigned char bit = 0; bit < 2; bit++)
+        for (bit_t bit = 0; bit < 2; bit++)
         {
-            encode_bit(0, &_state, &out);
-            trellis[state + (bit << polynome_count)] = out;
+            (void)encode_bit(0, state, &encbit);
+            trellis[state + (bit << polynome_count)] = encbit;
+        }
+    }
+    
+    char *bits = argv[argc-1];
+    int len = strlen(bits);
+#ifndef DEBUG
+    nodes = malloc(num_states * len / polynome_count * sizeof(struct Node));
+#endif
+    memset(nodes, 0, sizeof(nodes));
+    state_t state = 0;
+
+
+    struct Node *parents = &nodes[0];
+    parents[0].bits_ind = -1;
+    for (unsigned int i = 1; i < num_states; i++)
+    {
+        parents[i].metric = 0; // MAX_METRIC;
+        parents[i].state = i;
+        parents[i].bits_ind = -1;
+    }
+    struct Node *children = &nodes[num_states];
+
+    unsigned int *state_min_metric      = malloc(num_states * sizeof(unsigned int));
+    int *state_min_metric_ind  = malloc(num_states * sizeof(int));
+
+    unsigned int bit_ind = 0;
+    while (bit_ind < len)
+    {
+        for (int i = 0 ; i < num_states; i++)
+        {
+            state_min_metric[i] = MAX_METRIC;
+            state_min_metric_ind[i] = -1;
+        }
+
+        // pack in-encoded-bits
+        encbit_t in_encbit = 0;
+        for (int i = 0; i < polynome_count; i++)
+        {
+            bit_t bit = bits[bit_ind++] == '1';
+            in_encbit |= (bit << i);
+        }
+
+        // calculate next column of trellis
+        for (unsigned int i = 0; i < num_states; i++)
+        {
+            struct Node *parent = &parents[i]; 
+            for (bit_t bit = 0; bit < 2; bit++)
+            {
+                unsigned int child_ind = (i<<1) + bit;
+                struct Node *child = &children[child_ind];
+                state_t next_state = encode_bit(bit, parent->state, &encbit);
+                unsigned int ham_dist = hamming_dist(in_encbit, encbit);
+                chain_node(child, parent, bit, ham_dist, next_state);
+
+                if ( child->metric < state_min_metric[next_state] )
+                {
+                     state_min_metric[next_state] = child->metric;
+                     state_min_metric_ind[next_state] = child_ind;
+                }
+            }
+        }
+
+        // eliminate half of the children
+        for (unsigned int i = 0; i < num_states; i++)
+        {
+            parents[i] = children[state_min_metric_ind[i]];
+        }
+        bit_t q = 0;
+    }
+
+    unsigned int min_metric = MAX_METRIC;
+    unsigned int min_metric_ind = 0;
+    for (unsigned int i = 0; i < num_states; i++)
+    {
+        if ( parents[i].metric < min_metric )
+        {
+            min_metric = parents[i].metric;
+            min_metric_ind = i;
         }
     }
 
-    char *bits = argv[argc-1];
-    int len = strlen(bits);
-    int state = 0;
-    for (int i = 0; i < len; i++) {
-        unsigned char bit = bits[i] == '1';
-
+    struct Node chosen = parents[min_metric_ind];
+    for (int i = 0; i < chosen.bits_ind+1; i++) {
+        unsigned int int_num = i / NUM_BITS_IN_INT;
+        unsigned int bit_offset = i - int_num*NUM_BITS_IN_INT;  
+        unsigned int val = chosen.bits[int_num];
+        printf("%d", (bit_t)((val & (1<<bit_offset)) >> bit_offset));
     }
+    
+    printf("\n");
+    unsigned int q = 3;
 }
 
 
@@ -118,6 +265,8 @@ void decode(int argc, char *argv[]) {
 // ************************************************ //
 int main(int argc, char *argv[]) {
     
+    assert((MAX_DECODE_LEN_BITS / NUM_BITS_IN_INT) > 0);
+
     if ( argc == 1 ) {
         print_help();
         return 0;
--- unittests.py
+++ unittests.py
@@ -2,11 +2,31 @@ import os
 import subprocess
 import numpy as np
 from viterbi import Viterbi
+from tqdm import tqdm
+
+def binlen(x):
+    return len(bin(x))-2
 
 np.random.seed(0)
 polynomes = [0o133, 0o171] 
-baseline_viterbi = Viterbi(7, polynomes)
+#polynomes = [0b101, 0b110]
+
+depth = max(map(binlen, polynomes))
+baseline_viterbi = Viterbi(depth, polynomes)
 exe = os.path.join(os.getcwd(), "build", "viterbi_decoder")
+
+def encdec(bits_arr):
+    polys = [bin(p)[2:] for p in polynomes]
+    bits = "".join(map(str, bits_arr))
+    
+    enc = run(["-enc"] + polys + [bits])
+    dec = run(["-dec"] + polys + ["".join(map(str, enc))])
+    return list(map(int,enc)), list(map(int,dec))
+
+def baseline_encdec(bits_arr):
+    enc = baseline_viterbi.encode(bits_arr)
+    dec = baseline_viterbi.decode(enc)
+    return enc, dec
 
 def build():
     cmd = "mkdir -p build && cd build && cmake .. && make"
@@ -16,26 +36,21 @@ def run(argv):
     cmd = f"{exe} {' '.join(argv)}"
     ret = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return ret.stdout.strip()
-    
-def main():
-    bits_arr = np.random.randint(0,2, 50)
-    bits = "".join(map(str, bits_arr))
-    
-    print(f"\n{bin(polynomes[0])} {bin(polynomes[1])}")
-    print(f"\n{bits}\n")
-    
-    polys = [bin(p)[2:] for p in polynomes]
-    
-    z = baseline_viterbi.encode(bits_arr)
-    x = baseline_viterbi.decode(z)
-    
-    print("".join(map(str,z)))
-    print(run(["-enc"] + polys + [bits]))
 
-    print(" ")
-    
-    print("".join(map(str,x)))
-    print(run(["-dec"] + polys + [bits]))
+def gen_bits(n):
+    bits_arr = np.random.randint(0,2,n)
+    bits_arr[-depth:] = 0
+    return bits_arr
+
+def main():
+    for _ in tqdm(range(100)):
+        bits_arr = gen_bits(n=3000)
+        
+        bl_enc, bl_dec = baseline_encdec(bits_arr)
+        enc, dec = encdec(bits_arr)
+        
+        assert bl_enc == enc
+        assert bl_dec == dec
     
 if __name__ == "__main__":
     build()
